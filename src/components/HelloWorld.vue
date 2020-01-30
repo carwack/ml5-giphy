19be7aa449d9247a5d8d05c03fd17200315941b2
SybrenW1Dot <sybren.willemot@onedot.be>
2020-01-30 09:14:41 +0100

    Add axios and giphy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="hello">
-    <input type="file" name="uploadImage" @change="uploadImage" />
-    <img ref="image" alt="Vue logo" :src="img" />
     <div class="loading" v-if="!isModelLoaded">
       Model is loading...
     </div>
-    <div class="results" v-else>
-      {{ result.label }}
-      {{ result.confidence }}%
+    <div class="content" v-else>
+      <input type="file" name="uploadImage" @change="uploadImage" />
+      <div class="image-wrapper">
+        <img ref="image" alt="Vue logo" :src="img" />
+        <div class="results">
+          {{ result.label }}
+          {{ result.confidence }}%
+          <div style="width:100%;height:0;padding-bottom:125%;position:relative;">
+            <iframe
+              :src="result.giphy"
+              width="100%"
+              height="100%"
+              style="position:absolute"
+              frameBorder="0"
+              class="giphy-embed"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <p><a href="https://giphy.com/gifs/dancing-wtf-dope-TIXFmnKjrlMOP5zGNk">via GIPHY</a></p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import ml5 from 'ml5'
+import axios from 'axios'
 
 export default {
   name: 'HelloWorld',
@@ -23,7 +40,8 @@ export default {
       isModelLoaded: false,
       result: {
         label: '',
-        confidence: 0
+        confidence: 0,
+        giphy: ''
       },
       img: '@/assets/logo.png'
     }
@@ -48,6 +66,7 @@ export default {
           console.log(results)
           this.result.label = results[0].label
           this.result.confidence = (results[0].confidence * 100).toFixed(2)
+          this.getGiphy()
         }
       })
     },
@@ -59,6 +78,18 @@ export default {
         this.img = e.target.result
         this.classify()
       }
+    },
+    getGiphy: function() {
+      const url = 'https://api.giphy.com/v1/gifs/search?api_key=gnHx8iIxLiE3MLUDnVWJ6pcWDlI8LGqL&limit=1&q='
+      axios
+        .get(url + this.result.label + '&offset=' + Math.floor(Math.random() * 100 + 1))
+        .then((response) => {
+          console.log(response.data.data[0].embed_url)
+          this.result.giphy = response.data.data[0].embed_url
+        })
+        .catch((e) => {
+          this.errors.push(e)
+        })
     }
   }
 }
